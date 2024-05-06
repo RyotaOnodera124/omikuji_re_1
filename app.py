48cffbc94e7052f0272be65146943c562c4ec10e
RyotaOnodera124 <onoryo.0124@gmail.com>
2024-05-07 06:33:47 +0900

    おみくじアプリ完了

---
+++ app.py
@@ -0,0 +1,33 @@
+# import random
+
+# omikuji = ["大吉", "小吉", "吉"]
+
+# idx = random.randint(0, 2)
+# result = omikuji[idx]
+
+# print(f"今日の運勢は...{result}です！")
+
+
+# import random
+
+# omikuji = ["大吉", "小吉", "吉"]
+
+# idx = random.randint(0, 2)
+
+# print(f"今日の運勢は...{omikuji[idx]}")
+
+
+# import random
+
+# omikuji = ["超大吉", "大吉", "中吉", "小吉", "吉"]
+
+# idx = random.randint(0, len(omikuji) - 1)
+
+# print(f"今日の運勢は...{omikuji[idx]}です!")
+
+
+import random 
+
+omikuji = ["超大吉", "大吉", "中吉", "小吉", "吉"]
+
+print(f"今日の運勢は...{random.choice(omikuji)}")
